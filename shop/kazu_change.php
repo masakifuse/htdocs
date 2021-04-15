af257c1adc9a7f1e26c87bd6bd680f80713dfa4e
masakifuse <masaki.fuse1021@gmail.com>
2021-04-15 15:15:27 +0900

    first commit

---
+++ shop/kazu_change.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+session_regenerate_id(true);
+
+require_once('../common/common.php');
+
+$post=sanitize($_POST);
+
+$max=$post['max'];
+$kazu= [];
+$after=[];
+for($i=0;$i<$max;$i++)
+{
+	$kazu[]=$post['kazu'.$i];
+}
+$_SESSION['kazu']=$kazu;
+
+header('Location:shop_cartlook.php');
+exit();
+
+?>
+
+
+
